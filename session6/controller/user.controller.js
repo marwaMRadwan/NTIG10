e9cc83093ca45a13467753e043ef924d573b7921
marwaMRadwan <marwaradwan872@hotmail.com>
2021-09-13 11:28:41 +0200

    s6

--- session6/controller/user.controller.js
+++ session6/controller/user.controller.js
@@ -1,15 +1,23 @@
 
 const getAll = (req,res) => {
-    res.render('all')
+    res.render('all',{
+        pageTitle:"all Users"
+    })
 }
 const getSingle = (req,res) => {
-    res.render('single')
+    res.render('single',{
+        pageTitle:"Single User"
+    })
 }
 const editUser = (req,res) => {
-    res.render('edit')
+    res.render('edit',{
+        pageTitle:"Edit User"
+    })
 }
 const addUser = (req,res) => {
-    res.render('add')
+    res.render('add',{
+        pageTitle:"add user"
+    })
 }
 module.exports = {
     addUser, getAll, getSingle, editUser
