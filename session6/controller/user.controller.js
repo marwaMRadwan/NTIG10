7ac819230c1ee5005a5357a871388454a988ba2f
marwaMRadwan <marwaradwan872@hotmail.com>
2021-09-13 13:48:44 +0200

    s6

--- session6/controller/user.controller.js
+++ session6/controller/user.controller.js
@@ -1,24 +1,82 @@
-
-const getAll = (req,res) => {
-    res.render('all',{
-        pageTitle:"all Users"
+const fs = require('fs')
+const readJsonFile = () =>{
+    let allUsers
+    try{
+        allUsers = JSON.parse(fs.readFileSync('models/users.json').toString())
+        if(!Array.isArray(allUsers)) throw new Error()
+    }
+    catch(e){
+        allUsers = []
+    }
+    return allUsers
+}
+const saveJsonFile = (allUsers) =>{
+    fs.writeFileSync('models/users.json', JSON.stringify(allUsers))
+}
+const addUser = (req,res) => {
+    // if(req.query.name){
+    //     let user = req.query
+    //     // console.log(allUsers)
+    //     allUsers.push(user)
+    //     saveJsonFile(allUsers)
+    // }
+    res.render('add',{
+        pageTitle:"add user"
     })
 }
-const getSingle = (req,res) => {
-    res.render('single',{
-        pageTitle:"Single User"
+const saveUser = (req, res)=>{
+    // res.send(req.body)
+    let allUsers = readJsonFile()
+    let user = {
+        id: Date.now(),
+        ...req.body
+    }
+    allUsers.push(user)
+    saveJsonFile(allUsers)
+    res.redirect('/')
+}
+const getAll = (req,res) => {
+    let allUsers = readJsonFile()
+    res.render('all',{
+        pageTitle:"all Users",
+        data:allUsers
     })
+}
+searchUser = (allUsers, id) => {
+    let index = allUsers.findIndex(ele=>{
+        return ele.id==id
+    })
+    return index
+}
+const getSingle = (req,res) => {
+    let allUser = readJsonFile()
+    // let user = allUser.find(ele=>{
+    //     return ele.id==req.params.id
+    // })
+    // res.send(user)
+    let userIndex = searchUser(allUser, req.params.id)
+    if(userIndex==-1) res.render('err404', {
+        pageTitle:"User Not Found",
+        err: `No user With id ${req.params.id}`
+    })
+    else{
+        res.render('single',{
+            pageTitle:"Single User",
+            user: allUser[userIndex]
+        })    
+    }
 }
 const editUser = (req,res) => {
     res.render('edit',{
         pageTitle:"Edit User"
     })
 }
-const addUser = (req,res) => {
-    res.render('add',{
-        pageTitle:"add user"
+const err404 = (req,res)=>{
+    res.render('err404', {
+        pageTitle:"Error Page",
+        err: "not found"
     })
 }
 module.exports = {
-    addUser, getAll, getSingle, editUser
+    addUser, getAll, getSingle, editUser, saveUser, err404
 }
--- session6/routes/user.routes.js
+++ session6/routes/user.routes.js
@@ -1,8 +1,14 @@
 const router = require("express").Router()
 const userController = require('../controller/user.controller')
 
-router.get('', userController.getAll)
-router.get('/single', userController.getSingle)
 router.get('/addUser', userController.addUser)
-router.get('/editUser', userController.editUser)
+router.post('/addUser', userController.saveUser)
+
+router.get('', userController.getAll)
+
+router.get('/single/:id', userController.getSingle)
+router.get('/editUser/:id', userController.editUser)
+router.get('/del/:id', userController.getSingle)
+
+router.get('*', userController.err404)
 module.exports = router
