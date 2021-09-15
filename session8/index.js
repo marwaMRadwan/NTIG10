6cb53cbeea0dd2e85105725fb81d9dfa867f0bd0
marwaMRadwan <marwaradwan872@hotmail.com>
2021-09-15 11:24:01 +0200

    s8

---
+++ session8/index.js
@@ -0,0 +1,44 @@
+// npm init --y
+// npm i mongodb
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const {MongoClient, ObjectId} = require('mongodb')
+
+const dbURL = "mongodb://127.0.0.1:27017"
+const dbName = "g10DB"
+
+MongoClient.connect(dbURL, {}, (err, client)=>{
+    if(err) return console.log('unable to connect to db');
+
+    const db = client.db(dbName)
+
+    // db.collection('students').insertOne(
+    //     {name:"marwa", age:36},
+    //     (error, result)=>{
+    //         if(error) return console.log('error inserting data');
+    //         console.log('inserted success');
+    //         console.log(result)
+    //     }
+    // )
+
+    // db.collection('students').insertMany(
+    //     [{name:"marwa", age:36}, {name:"omar", age:6}, {name:"mazen",age:40},{name:"nouran", age:26} ],
+    //     (error, result)=>{
+    //         if(error) return console.log('error inserting data');
+    //         console.log('inserted success');
+    //         console.log(result.insertedCount)
+    //     }
+    // )
+
+db.collection('students').find().toArray((err,res)=>{
+    console.log(res)
+})
+
+db.collection('students').findOne(
+    {_id:new ObjectId("6141ae378f578fe45e7bdaaf")} ,
+    ( (error, res) => { console.log(res)} ) 
+    )
+
+})
