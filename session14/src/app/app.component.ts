9f2a6737711bf65267a064baf9397f19b4b587a8
marwaMRadwan <marwaradwan872@hotmail.com>
2021-09-23 15:07:59 +0200

    s14

---
+++ session14/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // counter = 1
+  // name = "marwa radwan"
+  // color = "red"
+  // cName = "testClass"
+  // names = ['a','b','c','d']
+
+  // x = "<h3>hello</h3>"
+  // y="<script>alert('test')</script>"
+  
+  // user:any = {
+  //   name:"marwa",
+  //   age:36
+  // }
+  // test(){
+  //   return 'hello'
+  // }
+  // handleClick(){
+  //   this.counter++;
+  //   console.log('hello')
+  // }
+  // handleClick1(){
+  //   console.log('hello1')
+  // }
+  // data:any
+  // handleInput(event:any){
+  //   console.log(event.target.value)
+  //   this.data=event.target.value
+  // }
+  // data1:any=""
+  // v:any=""
+
+
+
+}
