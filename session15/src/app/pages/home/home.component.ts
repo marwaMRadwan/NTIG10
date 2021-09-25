035d0dbfa2dcda37efbfee3d7d3239636aeb85b9
marwaMRadwan <marwaradwan872@hotmail.com>
2021-09-25 13:23:43 +0200

    s15

--- session15/src/app/pages/home/home.component.ts
+++ session15/src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from 'src/app/services/data.service'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // monthNum = 13
 
-  constructor() { }
+  // status: boolean = false
+  myData:any[]=[]
+  constructor(private _data:DataService) { }
 
   ngOnInit(): void {
+    this._data.getAllPosts().subscribe(data=>{
+      console.log(data)
+    })
   }
-
+// changeStatus(){
+//   this.status=!this.status
+// }
 }
