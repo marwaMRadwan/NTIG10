e4ed95c799197318d7b30131c12a5874b865d33b
marwaMRadwan <marwaradwan872@hotmail.com>
2021-09-26 12:54:22 +0200

    s16

--- session16/src/app/app-routing.module.ts
+++ session16/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { C1Component } from './components/c1/c1.component';
+import { RegisterComponent } from './forms/register/register.component';
 
 const routes: Routes = [
-  {path:"", component:C1Component}
+  // {path:"", component:C1Component}
+  {path:"", component:RegisterComponent}
 ];
 
 @NgModule({
--- session16/src/app/app.module.ts
+++ session16/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { C1Component } from './components/c1/c1.component';
 import { C2Component } from './components/c2/c2.component';
+import { RegisterComponent } from './forms/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     C1Component,
-    C2Component
+    C2Component,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CarouselModule.forRoot()
+    CarouselModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
